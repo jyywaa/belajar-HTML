e782dc0305f773a9bd5992957427fb900f21e4c6
jyywaa <kholifahrahmawati5@gmail.com>
2026-08-12 13:07:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const defaultProducts = [
         name: "Oppo A6 Pro",
         price: 15000000,
         desc: "Layar 120Hz OLED, RAM 12GB, Storage 256GB, Chipset Super Kencang.",
-        image: "Oppo.jpeg",
+        image: "oppo.jpeg",
         video: "1.mp4",
         rating: 4.9,
         sold: 142,
